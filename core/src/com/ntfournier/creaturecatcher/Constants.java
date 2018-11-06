440644aa6dc0aec727b04e5b44d05d9951076706
Vincent Fournier <vince@ntfournier.com>
2018-11-05 21:19:18 -0500

    Fix blurry zoom

--- core/src/com/ntfournier/creaturecatcher/Constants.java
+++ core/src/com/ntfournier/creaturecatcher/Constants.java
@@ -3,7 +3,7 @@ package com.ntfournier.creaturecatcher;
 public class Constants {
     public static final int TILE_SIZE = 16;
 
-    public static final float DEFAULT_ZOOM = 2f;
+    public static final float DEFAULT_ZOOM = 3f;
     public static final float DEFAULT_CAMERA_ZOOM = 1.f / DEFAULT_ZOOM;
 
     public static final int PLAYER_MOVEMENT_SPEED = TILE_SIZE * 2;
--- desktop/src/com/ntfournier/creaturecatcher/desktop/DesktopLauncher.java
+++ desktop/src/com/ntfournier/creaturecatcher/desktop/DesktopLauncher.java
@@ -1,13 +1,14 @@
 package com.ntfournier.creaturecatcher.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.*;
+import com.ntfournier.creaturecatcher.Constants;
 import com.ntfournier.creaturecatcher.CreatureCatcher;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.height = 16 * 20;
-        config.width = 16 * 20;
+        config.height = (int) (Constants.TILE_SIZE * Constants.DEFAULT_ZOOM * 9);
+        config.width = (int) (Constants.TILE_SIZE * Constants.DEFAULT_ZOOM * 10);
         new LwjglApplication(new CreatureCatcher(), config);
     }
 }
